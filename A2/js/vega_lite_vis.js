4d652eec5f7fd99e182d94de84c3203dcde5703d
kgan0017 <kgan0017@student.monash.edu>
2024-10-08 01:07:18 +1100

    added map and line, designed index.html

--- A2/js/vega_lite_vis.js
+++ A2/js/vega_lite_vis.js
@@ -1,5 +1,12 @@
 var vg_1 = "data/choropleth_map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {actions : false}).then(function(result) {
+    // Access the Vega view instance as result.view
+    // For more details, visit: https://vega.github.io/vega/docs/api/view/
+    var view = result.view;
+}).catch(console.error);
+
+var vg_2 = "data/bar.vg.json";
+vegaEmbed("#bar", vg_2, {actions : false}).then(function(result) {
     // Access the Vega view instance as result.view
     // For more details, visit: https://vega.github.io/vega/docs/api/view/
     var view = result.view;
