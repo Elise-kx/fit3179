63a2fd340ba2e9facbcf6fa5c593bf20a098ff80
kgan0017 <kgan0017@student.monash.edu>
2024-10-09 03:17:09 +1100

    fix visualisation design

--- A2/js/vega_lite_vis.js
+++ A2/js/vega_lite_vis.js
@@ -1,11 +1,11 @@
-var vg_1 = "data/choropleth_map.vg.json";
+var vg_1 = "js/choropleth_map.vg.json";
 vegaEmbed("#map", vg_1, {actions : false}).then(function(result) {
     // Access the Vega view instance as result.view
     // For more details, visit: https://vega.github.io/vega/docs/api/view/
     var view = result.view;
 }).catch(console.error);
 
-var vg_2 = "data/bar.vg.json";
+var vg_2 = "js/bar.vg.json";
 vegaEmbed("#bar", vg_2, {actions : false}).then(function(result) {
     // Access the Vega view instance as result.view
     // For more details, visit: https://vega.github.io/vega/docs/api/view/
