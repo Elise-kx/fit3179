d395a0df80938db1a25655d6409a228c2fd97d6c
kgan0017 <kgan0017@student.monash.edu>
2024-10-08 12:16:16 +1100

    added donut

--- A2/js/vega_lite_vis.js
+++ A2/js/vega_lite_vis.js
@@ -11,3 +11,10 @@ vegaEmbed("#bar", vg_2, {actions : false}).then(function(result) {
     // For more details, visit: https://vega.github.io/vega/docs/api/view/
     var view = result.view;
 }).catch(console.error);
+
+var vg_3 = "js/donut.vg.json";
+vegaEmbed("#donut", vg_3, {actions : false}).then(function(result) {
+    // Access the Vega view instance as result.view
+    // For more details, visit: https://vega.github.io/vega/docs/api/view/
+    var view = result.view;
+}).catch(console.error);
