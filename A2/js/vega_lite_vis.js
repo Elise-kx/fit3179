c3c139ff03dd4e18a79dfc48b3c9f2953ffc4809
kgan0017 <kgan0017@student.monash.edu>
2024-10-13 16:27:53 +1100

    added new charts

--- A2/js/vega_lite_vis.js
+++ A2/js/vega_lite_vis.js
@@ -1,20 +1,24 @@
 var vg_1 = "js/choropleth_map.vg.json";
 vegaEmbed("#map", vg_1, {actions : false}).then(function(result) {
-    // Access the Vega view instance as result.view
-    // For more details, visit: https://vega.github.io/vega/docs/api/view/
     var view = result.view;
 }).catch(console.error);
 
-var vg_2 = "js/bar.vg.json";
-vegaEmbed("#bar", vg_2, {actions : false}).then(function(result) {
-    // Access the Vega view instance as result.view
-    // For more details, visit: https://vega.github.io/vega/docs/api/view/
+var vg_2 = "js/top5_countries.vg.json";
+vegaEmbed("#top5_countries", vg_2, {actions : false}).then(function(result) {
     var view = result.view;
 }).catch(console.error);
 
 var vg_3 = "js/donut.vg.json";
 vegaEmbed("#donut", vg_3, {actions : false}).then(function(result) {
-    // Access the Vega view instance as result.view
-    // For more details, visit: https://vega.github.io/vega/docs/api/view/
+    var view = result.view;
+}).catch(console.error);
+
+var vg_4 = "js/percentage_increase.vg.json";
+vegaEmbed("#percentage_increase", vg_4, {actions : false}).then(function(result) {
+    var view = result.view;
+}).catch(console.error);
+
+var vg_5 = "js/visa_group.vg.json";
+vegaEmbed("#visa_group", vg_5, {actions : false}).then(function(result) {
     var view = result.view;
 }).catch(console.error);
