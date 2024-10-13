2def8b1824b1d247836ec0a6c4f39894b5b2b5c9
kgan0017 <kgan0017@student.monash.edu>
2024-10-13 19:37:52 +1100

    added dot plot to replace donut

--- A2/js/vega_lite_vis.js
+++ A2/js/vega_lite_vis.js
@@ -8,17 +8,12 @@ vegaEmbed("#top5_countries", vg_2, {actions : false}).then(function(result) {
     var view = result.view;
 }).catch(console.error);
 
-var vg_3 = "js/donut.vg.json";
-vegaEmbed("#donut", vg_3, {actions : false}).then(function(result) {
+var vg_3 = "js/dot.vg.json";
+vegaEmbed("#dot", vg_3, {actions : false}).then(function(result) {
     var view = result.view;
 }).catch(console.error);
 
-var vg_4 = "js/percentage_increase.vg.json";
-vegaEmbed("#percentage_increase", vg_4, {actions : false}).then(function(result) {
-    var view = result.view;
-}).catch(console.error);
-
-var vg_5 = "js/visa_group.vg.json";
-vegaEmbed("#visa_group", vg_5, {actions : false}).then(function(result) {
+var vg_4 = "js/gdp.vg.json";
+vegaEmbed("#gdp", vg_4, {actions : false}).then(function(result) {
     var view = result.view;
 }).catch(console.error);
